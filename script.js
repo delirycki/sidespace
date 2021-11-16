90b4147840bb9bfe0611c552278bf368dea237ac
Paweł Komarnicki <81227443+delirycki@users.noreply.github.com>
2021-11-16 15:08:02 +0100

    show information in div

---
+++ script.js
@@ -0,0 +1,110 @@
+const weightHTML = document.querySelector(".planets");
+const button = document.querySelector(".ripple");
+
+const buttons = document.querySelectorAll(".ripple");
+
+const planets = [
+  ["The Sun", 27.9, 365 / 12],
+
+  ["Mercury", 0.377, 88],
+  ["Venus", 0.9032, 225],
+  ["Earth", 1, 365.25],
+  ["Moon", 0.1655, 365.25],
+  ["Mars", 0.3895, 687],
+  ["Jupiter", 2.54, 11.8 * 365],
+  ["Saturn", 1.08, (29, 4 * 365)],
+  ["Uranus", 0.917, 84 * 365],
+  ["Neptune", 1.19, 164 * 365],
+  ["Pluto", 0.06, 248 * 365],
+];
+
+button.addEventListener("click", () => {
+  const massPerson = document.getElementById("weight").value;
+  const dateBirthday = document.getElementById("date").value;
+  weightHTML.innerHTML = "";
+
+  for (e in planets) {
+    const [year, months] = yourAgeInAnotherPlanet(dateBirthday, e);
+    const planetdiv = document.createElement("div");
+    planetdiv.classList.add("planet");
+    planetdiv.innerHTML = `
+        <strong>${planets[e][0]}</strong>
+        <p>Your weight is ${yourWaghtInAnotherPlanet(e, massPerson)} kg.</p>
+        <p>Your age is ${year} years and ${months} months old.</p>
+        <p>Your next birthday is ${nextBirthday(dateBirthday, e)}.</p>`;
+    weightHTML.appendChild(planetdiv);
+  }
+});
+
+function yourWaghtInAnotherPlanet(nrOfPlanet, massPerson) {
+  return Math.floor(planets[e][1] * massPerson);
+}
+
+function yourAgeInAnotherPlanet(dateBirthday, nrOfPlanet) {
+  const days = getNumberOfDays(dateBirthday, Date.now());
+
+  const yearsInSpace = days / planets[nrOfPlanet][2];
+  return [
+    Math.floor(yearsInSpace),
+    Math.floor(scale(yearsInSpace % 1, 0, 0.999999, 0, 11)),
+  ];
+}
+
+function getNumberOfDays(start, end) {
+  const date1 = new Date(start);
+  const date2 = new Date(end);
+
+  // One day in milliseconds
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  // Calculating the time difference between two dates
+  const diffInTime = date2.getTime() - date1.getTime();
+
+  // Calculating the no. of days between two dates
+  const diffInDays = Math.floor(diffInTime / oneDay);
+
+  return diffInDays;
+}
+
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+function nextBirthday(dateBirthday, nrOfPlanet) {
+  let date = new Date();
+
+  let daysToBirthday =
+    planets[nrOfPlanet][2] -
+    (getNumberOfDays(dateBirthday, Date.now()) % planets[nrOfPlanet][2]);
+  return addDays(Date.now(), daysToBirthday).toLocaleDateString();
+}
+
+function addDays(date, days) {
+  let result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+// Animation for button
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = yInside + window.scrollY + "px";
+    circle.style.left = xInside + "px";
+
+    this.appendChild(circle);
+
+    setTimeout(() => circle.remove(), 500);
+  });
+});
